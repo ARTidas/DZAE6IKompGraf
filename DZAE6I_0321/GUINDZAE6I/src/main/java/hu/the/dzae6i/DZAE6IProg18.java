fcef13f8fb1bfe922a2914930d112c3c63577506
Veres Zoltán <artidas@artidas.hu>
2024-04-25 21:11:48 +0200

    Added crude functions for the buttons, but glitches when clearing the background.

--- DZAE6I_0321/GUINDZAE6I/src/main/java/hu/the/dzae6i/DZAE6IProg18.java
+++ DZAE6I_0321/GUINDZAE6I/src/main/java/hu/the/dzae6i/DZAE6IProg18.java
@@ -4,11 +4,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class DZAE6IProg18 extends JFrame implements ActionListener {
     
     private Color backgroundColor = Color.WHITE;
     private Color drawColor = Color.BLACK;
+    private int mouseX, mouseY;
     
     public DZAE6IProg18() {
         setTitle("PTI");
@@ -17,17 +20,37 @@ public class DZAE6IProg18 extends JFrame implements ActionListener {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER));
         
-        JButton buttonDraw = new JButton("Draw");
-        buttonDraw.addActionListener(this);
-        getContentPane().add(buttonDraw);
+        JButton buttonClear = new JButton("Clear");
+        buttonClear.addActionListener(this);
+        getContentPane().add(buttonClear);
         
-        JButton buttonBackGroundColor = new JButton("SetBackgroundColor");
+        JButton buttonBackGroundColor = new JButton("Set Background Color");
         buttonBackGroundColor.addActionListener(this);
         getContentPane().add(buttonBackGroundColor);
         
-        JButton buttonDrawColor = new JButton("SetLineColor");
+        JButton buttonDrawColor = new JButton("Set Line Color");
         buttonDrawColor.addActionListener(this);
         getContentPane().add(buttonDrawColor);
+        
+        addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                mouseX = e.getX();
+                mouseY = e.getY();
+            }
+        });
+        
+        addMouseMotionListener(new MouseAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                Graphics g = getGraphics();
+                g.setColor(drawColor);
+                int x = e.getX();
+                int y = e.getY();
+                g.drawLine(mouseX, mouseY, x, y);
+                mouseX = x;
+                mouseY = y;
+            }
+        });
         
         setVisible(true);
     }
@@ -48,7 +71,8 @@ public class DZAE6IProg18 extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
 
         switch (e.getActionCommand()) {
-            case "Draw":
+            case "Clear":
+                clearDrawing();
                 break;
             case "Set Background Color":
                 backgroundColor = JColorChooser.showDialog(this, "Choose Color", backgroundColor);
@@ -60,7 +84,13 @@ public class DZAE6IProg18 extends JFrame implements ActionListener {
                 break;
         }
 
-        repaint();
+        //repaint();
+    }
+    
+    private void clearDrawing() {
+        Graphics g = getGraphics();
+        g.setColor(backgroundColor);
+        g.fillRect(0, 0, getWidth(), getHeight());
     }
     
     public static void main(String[] args) {
